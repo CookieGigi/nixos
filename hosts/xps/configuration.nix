486dcf930a22b641c80614cf6c914213df3410c9
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-11 06:10:16 +0200

    feat: media-session => wireplumber

--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -66,10 +66,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
