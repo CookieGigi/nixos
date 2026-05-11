f8ae8690d8c30e72bdb2467762b9f27ba8ee50d7
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-11 05:38:36 +0200

    feat: formatter

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@ inputs = {
 };
 
 outputs = { self, nixpkgs, ... }: {
+  formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
   nixosConfigurations.xps = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [ ./hosts/xps/configuration.nix ];
