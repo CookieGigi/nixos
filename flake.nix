22204f742aa1b1c8cf71a5501bad363189d6818a
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-11 05:56:18 +0200

    style: format

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-inputs = {
-  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-};
-
-outputs = { self, nixpkgs, ... }: {
-  formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-
-  nixosConfigurations.xps = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [ ./hosts/xps/configuration.nix ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-};
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+    nixosConfigurations.xps = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [./hosts/xps/configuration.nix];
+    };
+  };
 }
