a3536c02635c6d7d93fbca9afcc7e68ede6310bc
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-13 18:50:19 +0200

    feat: iso builder

--- flake.nix
+++ flake.nix
@@ -18,5 +18,19 @@
       system = "x86_64-linux";
       modules = [./hosts/xps/configuration.nix disko.nixosModules.disko];
     };
+
+    nixosConfigurations.xps-iso = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # 1. iso base
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        # 2. your real config (bring disko with it!)
+        ./hosts/xps/configuration.nix
+        # 3. disko module (need for disko.nix to work)
+        disko.nixosModules.disko
+        # 4. overrides (new file!)
+        ./hosts/xps/iso.nix
+      ];
+    };
   };
 }
--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -91,6 +91,8 @@
     package = pkgs.vim-full;
   };
 
+  programs.ssh.startAgent = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
