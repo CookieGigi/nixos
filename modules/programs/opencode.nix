fd44f735a57cb425924de72d4f3e2f2afc98e254
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-14 19:57:17 +0200

    feat: add impermanence persistence for opencode

--- modules/programs/opencode.nix
+++ modules/programs/opencode.nix
@@ -2,4 +2,14 @@
   environment.systemPackages = [
     pkgs.opencode
   ];
+
+  # Persist
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/home/cookiegigi/.config/opencode"
+      "/home/cookiegigi/.local/share/opencode"
+      "/home/cookiegigi/.local/state/opencode"
+    ];
+  };
 }
