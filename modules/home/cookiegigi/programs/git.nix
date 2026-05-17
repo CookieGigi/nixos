755b19286a608e632f68bda3e2b1454aa696a082
cookiegigi <cookiegigi@xps.cookiegigi.com>
2026-05-17 08:15:28 +0200

    feat: git config

--- modules/home/cookiegigi/programs/git.nix
+++ modules/home/cookiegigi/programs/git.nix
@@ -1,8 +1,26 @@
 {...}: {
   programs.git = {
     enable = true;
+
+    ignores = [".direnv/" "*.swp" ".DS_Store"];
+
     settings = {
       core.hooksPath = ".githooks";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      extraConfig.core.editor = "vi";
+      user.name = "cookiegigi";
+      user.email = "cookiegigi@cookiegigi.com";
+      gpg.format = "ssh";
+
+      aliases = {
+        unstage = "reset HEAD --";
+      };
+    };
+
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
     };
   };
 }
