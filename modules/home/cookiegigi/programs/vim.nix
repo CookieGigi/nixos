2c36f3a010bb1a58bc76c6aa8471f6e8f9bde6c7
cookiegigi <cookiegigi@cookiegigi.com>
2026-06-05 06:34:46 +0200

    feat: config vim-hardtime

--- modules/home/cookiegigi/programs/vim.nix
+++ modules/home/cookiegigi/programs/vim.nix
@@ -7,24 +7,27 @@
       vim-hardtime # blocks arrow/repeat hjkl abuse
     ];
     extraConfig = ''
-      set number           " absolute line
-      set relativenumber   " relative = see distance fast
+            set number           " absolute line
+            set relativenumber   " relative = see distance fast
 
-      " no arrow keys - learn hjkl
-      noremap <Up>    <Nop>
-      noremap <Down>  <Nop>
-      noremap <Left>  <Nop>
-      noremap <Right> <Nop>
-      inoremap <Up>    <Nop>
-      inoremap <Down>  <Nop>
-      inoremap <Left>  <Nop>
-      inoremap <Right> <Nop>
+            " no arrow keys - learn hjkl
+            noremap <Up>    <Nop>
+            noremap <Down>  <Nop>
+            noremap <Left>  <Nop>
+            noremap <Right> <Nop>
+            inoremap <Up>    <Nop>
+            inoremap <Down>  <Nop>
+            inoremap <Left>  <Nop>
+            inoremap <Right> <Nop>
 
-      " no mouse
-      set mouse=
-
-      " hardtime on by default
-      let g:hardtime_default_on = 1
+            " no mouse
+            set mouse= " hardtime on by default
+            let g:hardtime_default_on = 1
+            let g:hardtime_default_on = 1        " on for all buffers
+      let g:hardtime_timeout = 2000        " block window ms (default 2000)
+      let g:hardtime_maxcount = 2          " allow N repeats before block
+      let g:hardtime_allow_different_key = 1  " jkjkjk ok, jjj bad
+      let g:hardtime_showmsg = 1           " show message when blocked
     '';
   };
 
