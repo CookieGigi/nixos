86a9eb4a0c52b1d4584c637406459e1070a14bce
cookiegigi <cookiegigi@cookiegigi.com>
2026-06-14 06:41:56 +0200

    feat: set firefox as default

--- modules/home/cookiegigi/programs/firefox.nix
+++ modules/home/cookiegigi/programs/firefox.nix
@@ -136,4 +136,15 @@
       ".config/mozilla"
     ];
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
 }
