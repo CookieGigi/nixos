7b3fa391a66bd0d2bb24d63faeb0f466f39fee8d
cookiegigi <cookiegigi@cookiegigi.com>
2026-07-31 07:43:38 +0200

    feat(server): add unzip to system packages

--- modules/server/core-server.nix
+++ modules/server/core-server.nix
@@ -1,5 +1,9 @@
-{
+{pkgs, ...}: {
   # Server networking: systemd-networkd instead of NetworkManager
   networking.networkmanager.enable = false;
   networking.useNetworkd = true;
+
+  environment.systemPackages = with pkgs; [
+    unzip
+  ];
 }
