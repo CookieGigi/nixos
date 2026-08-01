24e36a24d6c8c25bb55345c0d4e367fc7250f761
cookiegigi <cookiegigi@cookiegigi.com>
2026-08-01 23:52:20 +0200

    refactor: remove unused container tool and dockerSocket

--- modules/server/containers.nix
+++ modules/server/containers.nix
@@ -1,15 +1,10 @@
-{pkgs, ...}: {
+_: {
   # Rootless Podman with Docker socket compatibility
   virtualisation.podman = {
     enable = true;
-    dockerSocket.enable = true;
   };
 
   environment = {
-    systemPackages = with pkgs; [
-      podman-compose
-    ];
-
     # Persist container storage (images, layers) across reboots on NVMe.
     persistence."/persist".directories = [
       "/var/lib/containers"
