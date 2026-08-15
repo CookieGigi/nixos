650c28bafc0b77a261aa296e239ea068b1d27a21
cookiegigi <cookiegigi@cookiegigi.com>
2026-08-15 09:25:47 +0200

    feat: dig

--- modules/common/programs/programs.nix
+++ modules/common/programs/programs.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./make.nix
+    ./dig.nix
     # All user-level program configurations migrated to home-manager
     # System-level program modules can be added here as needed
   ];
