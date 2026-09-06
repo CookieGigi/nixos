eb141425eac9d56e3e4dedd69fc47ab6bb80e258
cookiegigi <cookiegigi@cookiegigi.com>
2026-09-06 05:47:19 +0200

    fix: correct BookOrbit database ownership

--- modules/server/containers/bookorbit/filesystem.nix
+++ modules/server/containers/bookorbit/filesystem.nix
@@ -1,13 +1,17 @@
 {pkgs, ...}: {
   systemd.tmpfiles.rules = [
     "d /persist/bookorbit/app      0750 bookorbit media -"
-    "d /persist/bookorbit/postgres 0700 root root -"
+    "d /persist/bookorbit/postgres 0700 999 999 -"
     "d /media/documents/books      2775 root media -"
   ];
 
   system.activationScripts.bookorbit-ownership-migration = ''
     if [ -d /persist/bookorbit/app ]; then
       ${pkgs.coreutils}/bin/chown -R bookorbit:media /persist/bookorbit/app
+    fi
+
+    if [ -d /persist/bookorbit/postgres ]; then
+      ${pkgs.coreutils}/bin/chown -R 999:999 /persist/bookorbit/postgres
     fi
   '';
 }
