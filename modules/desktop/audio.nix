084cb1adc942d0af10babc9ac362a42e390a40a9
cookiegigi <cookiegigi@cookiegigi.com>
2026-09-20 06:43:13 +0200

    fix: auto-connect MOMENTUM TW 4 audio profiles

--- modules/desktop/audio.nix
+++ modules/desktop/audio.nix
@@ -7,6 +7,19 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig."51-momentum-tw4" = {
+        "monitor.bluez.rules" = [
+          {
+            matches = [{"device.name" = "bluez_card.80_C3_BA_7C_C7_05";}];
+            actions.update-props = {
+              # Complete audio profile connections when the earbuds only partially reconnect.
+              "bluez5.auto-connect" = ["a2dp_sink" "hfp_hf" "hsp_hs"];
+            };
+          }
+        ];
+      };
+    };
   };
 }
